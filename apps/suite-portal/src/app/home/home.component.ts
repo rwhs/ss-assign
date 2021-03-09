ee826362aba6c8fc38994de72bc7c621c2f819bd
rwhs <ronald.so1996@gmail.com>
2021-03-09 15:22:09 -0500

    feat: Implemented maintenance request form

--- apps/suite-portal/src/app/home/home.component.ts
+++ apps/suite-portal/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ALL_SERVICE_TYPES } from '@suiteportal/api-interfaces';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'pm-home',
@@ -10,10 +13,46 @@ export class HomeComponent implements OnInit {
 
   serviceTypes = ALL_SERVICE_TYPES;
 
-  constructor() {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
     //
   }
 
+  maintenanceForm = this.fb.group({
+    unitNumber: ['', Validators.required],
+    name: ['', Validators.required],
+    email: ['', Validators.email],
+    serviceType: ['', Validators.required],
+    summary: ['', Validators.required],
+    details: ['']
+  })
+
+  onSubmit() {
+
+    // const formData: any = new FormData();
+
+    // Populate FormData
+    // for (const element in this.maintenanceForm.value) {
+    //   // console.log(element + ': ' + this.maintenanceForm.value[element])
+    //   formData.append(element, this.maintenanceForm.value[element])
+    // }
+
+    // Only send POST request if form is valid
+    if (this.maintenanceForm.valid) {
+      console.log(this.maintenanceForm.value)
+      this.http.post('http://localhost:3333/api/maintenance-requests/', this.maintenanceForm.value).subscribe(
+        (response) => console.log(response),
+        // {
+        //   if (response) {
+        //     this.maintenanceForm.reset()
+        //   }
+        // },
+        (error) => console.log(error)
+      )
+    }
+  }
   ngOnInit(): void {
     //
   }
--- apps/suite-portal/src/app/home/home.module.ts
+++ apps/suite-portal/src/app/home/home.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [HomeComponent],
   exports: [HomeComponent]
