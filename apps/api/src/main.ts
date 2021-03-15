979de4a3bc2f25621d7a4bea4bf5e9db28ac3780
rwhs <ronald.so1996@gmail.com>
2021-03-15 14:42:42 -0400

    feat: Initialize Swagger/OpenAPI

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,24 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('SuitePortal')
+    .setDescription('SuitePortal API')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
