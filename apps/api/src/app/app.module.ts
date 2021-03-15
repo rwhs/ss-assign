4a6156e5eb061a792d21e60994bad98d01dd1872
rwhs <ronald.so1996@gmail.com>
2021-03-15 13:28:43 -0400

    style: semicolons

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { MaintenanceRequestModule } from '../maintenance-request/maintenance-request.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from '../auth/auth.module'
-import { AdminModule } from '../admin/admin.module'
+import { AuthModule } from '../auth/auth.module';
+import { AdminModule } from '../admin/admin.module';
 
 @Module({
   imports: [MaintenanceRequestModule, AuthModule, AdminModule],
